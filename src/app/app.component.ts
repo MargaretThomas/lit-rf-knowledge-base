7f1123fdbafd6b25fb905a28636c39bd1c9dbe32
Margaret Thomas <margaret.roberta.thomas@gmail.com>
2024-02-08 13:04:59 +0200

    Add base layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideNavigationBarComponent } from "./side-navigation-bar/side-navigation-bar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, SideNavigationBarComponent]
 })
 export class AppComponent {
   title = 'lit-rf-knowledge-base';
