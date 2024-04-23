4930e9364a8100fec2020d07e425101d70bfec36
Margaret <margaret.roberta.thomas@gmail.com>
2024-04-24 00:58:37 +0200

    Add floating action button for adding new space capsules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNavigationBarComponent } from "./side-navigation-bar/side-navigation-bar.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewSpaceCapsuleFabComponent } from "./new-space-capsule-fab/new-space-capsule-fab.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SideNavigationBarComponent, DashboardComponent]
+    imports: [RouterOutlet, SideNavigationBarComponent, DashboardComponent, NewSpaceCapsuleFabComponent]
 })
 export class AppComponent {
   title = 'lit-rf-knowledge-base';
